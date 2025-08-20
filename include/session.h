2e2cf9892fe5924e72ab5c110a3c1d3c3113c699
Ferdinand <atiferdinand31@gmail.com>
2025-08-20 15:04:49 +0100

    mod des fichiers de session

--- include/session.h
+++ include/session.h
@@ -1,5 +1,21 @@
 #ifndef __SESSION_H__
 #define __SESSION_H__
+#include <boost/asio.hpp>
+#include <memory>
 
+using boost::asio::ip::tcp;
+
+class Session
+{
+    public:
+      Session(tcp::socket);
+      void start();
+
+    private: 
+      tcp::socket _socket;
+      std::shared_ptr<std::array<char,1024>> _buffer;
+      void _do_read();
+      
+};
 
 #endif
--- src/session.cpp
+++ src/session.cpp
@@ -1,1 +1,22 @@
 #include "../include/session.h"
+
+Session::Session(tcp::socket socket): _socket(std::move(socket)){
+    _buffer = std::make_shared<std::array<char,1024>>();
+}
+
+void Session::start()
+{
+    _do_read();
+}
+
+void Session::_do_read()
+{
+    _socket.async_read_some(
+        boost::asio::buffer(*_buffer),[&](const boost::system::error_code &err,std::size_t data_length){
+            if(!err)
+            {
+                _do_read();
+            }
+        }
+    );
+}
