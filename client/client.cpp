f7a0f694bf09c503fe19d1bec22fa4bc2236ed97
Ferdinand <atiferdinand31@gmail.com>
2025-09-02 23:16:56 +0100

    amélioration du client

--- client/client.cpp
+++ client/client.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <vector>
+#include <fstream>
 
-int main() {
+std::vector<unsigned char> get_pdf_data()
+{
+    std::ifstream pdf_file("test.pdf",std::ios::binary);
+    if(!pdf_file)
+    {
+        std::cerr << "Impossible d'ouvrir le fichier test.pdf"<<std::endl;
+        exit(1);
+    }
+    std::vector<unsigned char> pdf_data((std::istreambuf_iterator<char>(pdf_file)),std::istreambuf_iterator<char>());
+    pdf_data.push_back('\n');
+    return pdf_data;
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 3)
+    {
+        std::cerr << "Usage : ./client your_id dest_id"<<std::endl;
+        return 1;
+    }
+    std::string your_id = argv[1]; 
+    std::string dest_id = argv[2];
     try {
         boost::asio::io_context io_context;
 
         // Résolution du serveur et port
         boost::asio::ip::tcp::resolver resolver(io_context);
-        auto endpoints = resolver.resolve("127.0.0.1", "1234"); // IP et port du serveur
+        auto endpoints = resolver.resolve("127.0.0.1", "12345"); // IP et port du serveur
 
         // Création et connexion du socket
         boost::asio::ip::tcp::socket socket(io_context);
@@ -15,9 +37,8 @@ int main() {
 
         std::cout << "Connecté au serveur !" << std::endl;
 
-        // Envoi d'un message
-        std::string id = "235";
-        boost::asio::write(socket, boost::asio::buffer(id));
+        // Envoi de son id
+        boost::asio::write(socket, boost::asio::buffer(your_id+"\n"));
 
         // Lecture de la réponse du serveur
         char reply[1024];
@@ -25,13 +46,21 @@ int main() {
         std::cout << "Réponse du serveur : ";
         std::cout.write(reply, reply_length);
         std::cout << std::endl;
-
-        if(reply == "OK")
+        std::string response(reply,reply_length);
+        if(response == "OK\n")
         {
+            std::vector<unsigned char> pdf_data = get_pdf_data();
+            std::string send_msg(pdf_data.begin(),pdf_data.end());
+            std::cout << send_msg << std::endl;
+            boost::asio::write(socket, boost::asio::buffer(send_msg));   
+            size_t reply_length_ = socket.read_some(boost::asio::buffer(reply, 1024));
+            std::cout.write(reply,reply_length_);
+            std::cout << "\n";
         }
         else
         {
             std::cerr << "Le serveur n'a pas accepté votre connexion" << std::endl;
+            return 1;
         }
 
     } catch (std::exception& e) {
